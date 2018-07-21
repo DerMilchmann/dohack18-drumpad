5e08d5b8e6376e488b8bd6deeb7f720a595d29cb
DerMilchmann <jakob.rhb@googlemail.com>
2018-07-21 15:38:44 +0200

    Takt class added

--- app/src/main/java/de/dohack/dohack18/drumpad/activities/MainActivity.java
+++ app/src/main/java/de/dohack/dohack18/drumpad/activities/MainActivity.java
@@ -2,14 +2,26 @@ package de.dohack.dohack18.drumpad.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.*;
 
 import com.example.jakob.drumpad.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Takt takt;
+    Timer timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        setContentView(R.layout.activity_main);
+        takt = new Takt(4,4);
     }
 }
